6fd10973d8976c5ff2c4b3f5a7518c757592cf3f
Altaf-7 <altafraja010789@gmail.com>
2026-06-23 10:16:36 +0530

    Exposed a basic FastAPI endpoint

---
+++ python/app/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+# from fastapi import HTTPException
+from pydantic import BaseModel
+from scraper import fetch_website_content
+
+app = FastAPI(
+    title="AI Knowledge Extractor Scraper Service",
+    version="1.0.0"
+)
+
+class URLRequest(BaseModel):
+    url:str
+
+@app.post("/scrape")
+def scrape_website(data:URLRequest):
+    result = fetch_website_content(data.url)
+    return result
