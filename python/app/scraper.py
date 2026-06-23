6949ad72ecc22445d226413e6e70fe31d91195a8
Altaf-7 <altafraja010789@gmail.com>
2026-06-23 10:07:18 +0530

    Added Scraper.py

---
+++ python/app/scraper.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {"User-Agent":"Mozilla/5.0"}
+
+def fetch_website_content(url:str):
+    """
+    Returns the json file containing title and content
+    """
+    
+    response = requests.get(url,headers=headers,timeout=10)
+
+    soup = BeautifulSoup(response.text,'html.parser')
+
+    title = soup.title.string if soup.title else "No title found"
+
+    if soup.body:
+        for irrelevant in soup.body(["script", "style", "img", "input"]):
+            irrelevant.decompose()
+        text = soup.body.get_text(separator="\n", strip=True)
+    else:
+        text = ""
+
+    return {
+        "title":title,
+        "content":text[:2_000]
+    }
+
+
+if __name__ == "__main__":
+    url = "http://example.com"
+    res = fetch_website_content(url)
+    print(res)
